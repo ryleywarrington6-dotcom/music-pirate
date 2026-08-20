6939a06cf3e97e9de396a4525f6c5fef2b6179e3
Ryley Graham-Warrington <251774574+ryleywarrington6-dotcom@users.noreply.github.com>
2026-08-20 16:24:12 +0000

    modified:   streamer.py

--- streamer.py
+++ streamer.py
@@ -2077,3 +2077,4 @@ def serve_profiles(filename):
 if __name__ == '__main__':
     print(f"🎵 App running on port {PORT}! Open http://localhost:{PORT}")
     app.run(host='0.0.0.0', port=PORT)
+    
